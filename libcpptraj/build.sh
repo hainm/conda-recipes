d811278b11b18fccafaeb5fd12b8ebe450d51924
Hai Nguyen <hainm.comp@gmail.com>
2015-10-10 00:52:24 -0400

    add -j8 cpptraj

--- libcpptraj/build.sh
+++ libcpptraj/build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 bash ./configure -shared -nomathlib --with-netcdf=$PREFIX -openmp gnu
-make libcpptraj
+make libcpptraj -j8
+make install -j8
 
 mkdir -p $PREFIX/include/cpptraj/
 
 cp lib/libcpptraj* $PREFIX/lib/
+cp bin/cpptraj* $PREFIX/bin/
 
 cp src/*.h $PREFIX/include/cpptraj/
