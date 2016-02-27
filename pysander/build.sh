0b8a69e41c284d571c8def1757f48f6db1f5ba73
Jason Swails <jason.swails@gmail.com>
2016-02-27 01:41:42 -0500

    Fix pysander build for macs.

--- pysander/build.sh
+++ pysander/build.sh
@@ -7,12 +7,20 @@ fail() {
 }
 
 test -z $AMBERHOME && fail "Missing AMBERHOME"
-test -f "$AMBERHOME/lib/libsander.so" || fail "Missing libsander.so"
-test -f "$AMBERHOME/lib/libsanderles.so" || fail "Missing libsanderles.so"
+
+if [ `uname` = "Darwin" ]; then
+    test -f "$AMBERHOME/lib/libsander.dylib" || fail "Missing libsander.dylib"
+    test -f "$AMBERHOME/lib/libsanderles.dylib" || fail "Missing libsanderles.dylib"
+    mkdir -p $PREFIX/lib && \
+        cp "$AMBERHOME/lib/libsander.dylib" "$AMBERHOME/lib/libsanderles.dylib" $PREFIX/lib
+else
+    test -f "$AMBERHOME/lib/libsander.so" || fail "Missing libsander.so"
+    test -f "$AMBERHOME/lib/libsanderles.so" || fail "Missing libsanderles.so"
+    mkdir -p $PREFIX/lib && \
+        cp "$AMBERHOME/lib/libsander.so" "$AMBERHOME/lib/libsanderles.so" $PREFIX/lib
+fi
 test -f "$AMBERHOME/include/sander.h" || fail "Missing sander.h"
 
-mkdir -p $PREFIX/lib && \
-    cp "$AMBERHOME/lib/libsander.so" "$AMBERHOME/lib/libsanderles.so" $PREFIX/lib
 mkdir -p $PREFIX/include && cp "$AMBERHOME/include/sander.h" $PREFIX/include
 
 $PYTHON setup.py install
