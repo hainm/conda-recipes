8b6cb2d7d31b09323fcc66c716901b92f85838dc
Jason Swails <jason.swails@gmail.com>
2016-02-27 01:04:34 -0500

    Fix up the recipe.

--- pysander/build.sh
+++ pysander/build.sh
@@ -11,7 +11,8 @@ test -f "$AMBERHOME/lib/libsander.so" || fail "Missing libsander.so"
 test -f "$AMBERHOME/lib/libsanderles.so" || fail "Missing libsanderles.so"
 test -f "$AMBERHOME/include/sander.h" || fail "Missing sander.h"
 
-mkdir -p $PREFIX/lib && cp "$AMBERHOME/lib/libsander*.so" $PREFIX/lib
+mkdir -p $PREFIX/lib && \
+    cp "$AMBERHOME/lib/libsander.so" "$AMBERHOME/lib/libsanderles.so" $PREFIX/lib
 mkdir -p $PREFIX/include && cp "$AMBERHOME/include/sander.h" $PREFIX/include
 
-python setup.py install
+$PYTHON setup.py install
