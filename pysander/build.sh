32d9cdb6d3a5177b433845061b9934d3ca664cbd
Jason Swails <jason.swails@gmail.com>
2016-02-27 00:30:40 -0500

    Add a recipe for building pysander.

---
+++ pysander/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Assume we already have Amber installed -- so copy the
+fail() {
+    echo "$1"
+    exit 1
+}
+
+test -z $AMBERHOME || fail "Missing AMBERHOME"
+test -f "$AMBERHOME/lib/libsander.so" || fail "Missing libsander.so"
+test -f "$AMBERHOME/lib/libsanderles.so" || fail "Missing libsanderles.so"
+test -f "$AMBERHOME/include/sander.h" || fail "Missing sander.h"
+
+mkdir -p $PREFIX/lib && cp "$AMBERHOME/lib/libsander*.so" $PREFIX/lib
+mkdir -p $PREFIX/include && cp "$AMBERHOME/include/sander.h" $PREFIX/include
+
+python setup.py install
