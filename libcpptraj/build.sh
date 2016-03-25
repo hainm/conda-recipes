595a7ade0df46dee6a0526930cf7185b4e49a765
Hai Nguyen <hainm.comp@gmail.com>
2016-03-25 02:23:07 -0400

    test

--- libcpptraj/build.sh
+++ libcpptraj/build.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-bash configure -shared -macAccelerate --with-fftw3=/usr/local --with-netcdf=/usr/local -noarpack clang
+PLATFORM=`uname -s | awk '{print $1}'`
+
+if [[ $PLATFORM = "Darwin" ]] ; then
+    bash configure -shared -macAccelerate --with-fftw3=/usr/local --with-netcdf=/usr/local -noarpack clang
+else
+    bash ./configure --with-netcdf=$PREFIX \
+                     --with-blas=$PREFIX \
+                     --with-bzlib=$PREFIX \
+                     --with-zlib=$PREFIX \
+                     -openmp \
+                     -shared \
+                     -openblas -noarpack gnu
+fi
 make libcpptraj -j${CPU_COUNT}
 
 mkdir -p $PREFIX/include/cpptraj/
