075c4dc429025366b275bd4e31504a3e2e008876
Hai Nguyen <hainm@users.noreply.github.com>
2016-05-20 04:10:14 -0400

    Add files via upload
    initial attempt. Can build libcpptraj now but still need to figure how to use 'mkdir'

---
+++ libcpptraj/bld.bat
@@ -0,0 +1,15 @@
+bash ./configure ^
+                 --with-zlib=$PREFIX ^
+		 -nobzlib ^
+		 -nomathlib ^
+		 -nonetcdf ^
+                 -shared ^
+		 -noreadline ^
+                 -noarpack gnu
+
+c:\mingw32\bin\mingw32-make.exe libcpptraj -j8
+
+mkdir -p $PREFIX/include/cpptraj/
+
+cp lib/libcpptraj* $PREFIX/lib/
+cp src/*.h $PREFIX/include/cpptraj/
